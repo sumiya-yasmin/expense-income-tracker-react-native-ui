31453ba2ec44709d194b8af2179ff42058f4f44f
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-26 20:18:00 +0600

    add index page for auth

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,13 +1,21 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { Link, router } from "expo-router";
+import { useEffect } from "react";
+import { Image, Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      router.replace("/(auth)/onboarding");
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
-    <View
-      className="flex-1 justify-center items-center"
-    >
-      <Text className="text-green-700 text-5xl">BudgetIQ</Text>
-      <Link href="/onboarding">Onboarding</Link>
+    <View className="flex-1 justify-center items-center">
+      <Image
+        source={require("../../assets/images/splashicon.png")}
+        className="w-52 h-52"
+        resizeMode="contain"
+      />
     </View>
   );
 }
