2ad9a1cb7eb5da50fbc0bbba60d0d912d39095d1
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-27 02:36:24 +0600

    Add back button component

---
+++ components/BackButton.tsx
@@ -0,0 +1,17 @@
+import { Text, TouchableOpacity } from "react-native";
+import { Ionicons } from '@expo/vector-icons'; 
+
+
+const BackButton = ({ onPress }: { onPress: () => void }) => (
+  <TouchableOpacity
+    onPress={onPress}
+    className="z-10 mb-2"
+  >
+    <Ionicons 
+        name="chevron-back" 
+        size={32}           
+        color="#6B7280"     
+    />
+  </TouchableOpacity>
+);
+export default BackButton;
