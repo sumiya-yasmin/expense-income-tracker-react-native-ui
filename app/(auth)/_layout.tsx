d0ffdb444419f5041a420b5be0b2051d5544fd8e
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-26 20:11:02 +0600

    add auth layout

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,5 +1,17 @@
 import { Stack } from "expo-router";
 
 export default function AuthLayout() {
-  return <Stack />;
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="onboarding" />
+      <Stack.Screen name="login-signup-select" />
+      <Stack.Screen name="login" />
+      <Stack.Screen name="signup" />
+      <Stack.Screen name="account-verification" />
+      <Stack.Screen name="forgot-password" />
+      <Stack.Screen name="otp" />
+      <Stack.Screen name="new-password" />
+    </Stack>
+  );
 }
