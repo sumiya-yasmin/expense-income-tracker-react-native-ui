4e5eb8757750d75dfcde85422cc3c91aff431464
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-27 02:41:18 +0600

    Add account verification page

--- app/(auth)/account-verification.tsx
+++ app/(auth)/account-verification.tsx
@@ -1,12 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from "react";
+import VerificationOTPScreen from "@/components/VerificationOTPScreen";
+import { router } from "expo-router";
 
 const AccountVerification = () => {
+  const handleVerification = (otpCode: string) => {
+    router.replace("/(auth)/login");
+  };
   return (
-    <View>
-      <Text>account Verification</Text>
-    </View>
-  )
-}
+    <VerificationOTPScreen
+      title="Account Verification"
+      description="To verify you account, please enter the verification code that you have received in your email co***ty@gmail.com"
+      onConfirm={handleVerification}
+      showBackButton={false}
+    />
+  );
+};
 
 export default AccountVerification;
