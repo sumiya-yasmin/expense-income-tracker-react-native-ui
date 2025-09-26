cef49151c24b7e967c6dc3091650d2750faa037c
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-26 23:41:55 +0600

    Add onboarding  pages

--- app/(auth)/login-signup-select.tsx
+++ app/(auth)/login-signup-select.tsx
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import OnboardingSlide from '@/components/OnboardlingSlide'
+import { router } from 'expo-router'
 
 const LoginSignupSelect = () => {
-  return (
-    <View>
-      <Text>LoginSignupSelect</Text>
-    </View>
-  )
+ const IMAGE_SOURCE: ImageSourcePropType = require("../../assets/images/onboarding2.png");
+   const handleNext = ()=>{
+     router.push("/(auth)/login");
+   }
+   
+   return (
+     <OnboardingSlide
+      imageSource={IMAGE_SOURCE}
+      title="Best Budgeting Tools for Home Finances"
+      description="Easily log in or sign up to connect with BUDGET IQ and calculate your daily cost."
+      onNext={handleNext}
+      buttonText = "Next"
+      slideIndex={1}
+     />
+   )
 }
 
 export default LoginSignupSelect
 
-const styles = StyleSheet.create({})
