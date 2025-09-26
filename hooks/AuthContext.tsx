0fddaba3f354d4edc031cef516255679e9628c4d
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-26 19:56:08 +0600

    Add auth context

---
+++ hooks/AuthContext.tsx
@@ -0,0 +1,47 @@
+import { router } from "expo-router";
+import { createContext, ReactNode, useContext, useEffect, useState } from "react";
+
+interface AuthContextType {
+  isAuthenticated: boolean;
+  signIn: () => void;
+  signOut: () => void;
+  isLoading: boolean;
+}
+
+const AuthContext = createContext<AuthContextType>({
+  isAuthenticated: false,
+  signIn: () => {},
+  signOut: () => {},
+  isLoading: true,
+});
+
+export function AuthProvider({children}:{children: ReactNode}){
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
+ useEffect(()=>{
+   setTimeout(()=>{
+    setIsLoading(false);
+   },1000);
+ },[])
+
+
+const signIn = () =>{
+    setIsAuthenticated(true);
+    router.replace('/(app)');
+}
+
+const signOut = () => {
+    setIsAuthenticated(false);
+    router.replace('/(auth)'); 
+  };
+
+return(
+    <AuthContext.Provider value={{ isAuthenticated, signIn, signOut, isLoading }}>
+        {children}
+    </AuthContext.Provider>
+)
+
+}
+
+export const useAuth = () => useContext(AuthContext);
