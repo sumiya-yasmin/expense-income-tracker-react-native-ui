86ce78adebab4f520b845f268a65d811fa6e7ab9
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-26 19:57:54 +0600

    Add app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,42 @@
-import './globals.css';
+import { AuthProvider, useAuth } from "@/hooks/AuthContext";
+import "./globals.css";
+import * as SplashScreen from "expo-splash-screen";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 
-export default function RootLayout() {
-  return <Stack />;
+SplashScreen.preventAutoHideAsync();
+
+function RootLayoutContent() {
+  const { isAuthenticated, isLoading } = useAuth();
+  useEffect(() => {
+    if (!isLoading) {
+      SplashScreen.hideAsync();
+    }
+  }, [isLoading]);
+  if (isLoading) {
+    return <Stack screenOptions={{ headerShown: false }} />;
+  }
+  return (
+    <Stack>
+      <Stack.Screen
+        name="(auth)"
+        options={{ headerShown: false }}
+        redirect={isAuthenticated}
+      />
+       <Stack.Screen
+        name="(app)"
+        options={{ headerShown: false }}
+        redirect={!isAuthenticated}
+      />
+      
+    </Stack>
+  );
+}
+
+export default function RootLayout(){
+  return(
+    <AuthProvider>
+      <RootLayoutContent/>
+    </AuthProvider>
+  )
 }
