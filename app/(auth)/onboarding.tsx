0546e2a258b13cf1468e9cd471a6bf52a7c63045
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-26 23:41:28 +0600

    Add index page

--- app/(auth)/onboarding.tsx
+++ app/(auth)/onboarding.tsx
@@ -1,14 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageSourcePropType, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import OnboardingSlide from '@/components/OnboardlingSlide'
+import { router } from 'expo-router';
 
 const Onboarding = () => {
+  const IMAGE_SOURCE: ImageSourcePropType = require("../../assets/images/onboarding1.png");
+  const handleNext = ()=>{
+    router.push("/(auth)/login-signup-select");
+  }
+  
   return (
-    <View>
-      <Text>onboarding</Text>
-    </View>
+    <OnboardingSlide
+     imageSource={IMAGE_SOURCE}
+     title="Your Trusted Financial Advisor — Always Within Reach"
+     description="Connect instantly with savings experts — right from your phone."
+     onNext={handleNext}
+     buttonText = "Next"
+     slideIndex={0}
+    />
   )
 }
 
 export default Onboarding
 
-const styles = StyleSheet.create({})
