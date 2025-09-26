d0156c34a8c3cc6d39a99f05890dad9b5d3d1e01
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-26 15:19:49 +0600

    add basic auth navigation and routes

---
+++ app/(auth)/login-signup-select.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoginSignupSelect = () => {
+  return (
+    <View>
+      <Text>LoginSignupSelect</Text>
+    </View>
+  )
+}
+
+export default LoginSignupSelect
+
+const styles = StyleSheet.create({})
