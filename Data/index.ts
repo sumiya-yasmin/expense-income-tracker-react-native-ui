5d80a29d8952d87fa67000e7236f779040d0a160
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-27 23:59:58 +0600

    Add mock data

---
+++ Data/index.ts
@@ -0,0 +1,29 @@
+export const MOCK_EXPENSES = [
+  { id: '1', icon: 'home-outline', name: 'Home Rent', amount: 500, date: '02 June, 2025' },
+  { id: '2', icon: 'car-side', name: 'Car Fuel', amount: 100, date: '05 June, 2025' },
+  { id: '3', icon: 'food', name: 'Food', amount: 450, date: '05 June, 2025' },
+  { id: '4', icon: 'wrench', name: 'Car Maintaining', amount: 250, date: '12 June, 2025' },
+ { id: '5', icon: 'gift', name: 'Gift', amount: 100, date: '07 June, 2025' },
+];
+
+export const MOCK_INCOME = [
+  { id: '5', icon: 'cash', name: 'Company Salary', amount: 1500, date: '01 June, 2025' },
+  { id: '6', icon: 'gift', name: 'Gift', amount: 100, date: '07 June, 2025' },
+];
+
+export const EXPENSE_CATEGORY_ICONS = [
+    { name: 'Home', icon: 'home-outline', amount: '$500' },
+    { name: 'Travel', icon: 'airplane', amount: '$450' },
+    { name: 'Car', icon: 'car-outline', amount: '$350' },
+    { name: 'Food', icon: 'food', amount: '$350' },
+    {name: 'Car Maintaining', icon: 'wrench',  amount: '$250',},
+    {name: 'Car Fuel',icon: 'car-side', amount: '$100',},
+    {name: 'Gift',icon: 'gift',  amount: '$100',}
+];
+
+
+export const INCOME_CATEGORY_ICONS = [
+    { name: 'Home', icon: 'home-outline', amount: '$500' },
+    {name: 'Company Salary',icon: 'cash',  amount: '$1500',},
+    {name: 'Gift',icon: 'gift',  amount: '$100',}
+];
