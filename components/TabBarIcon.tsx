ed11247d55e026b8346e609b11d79b75bfdbaedb
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-28 19:03:25 +0600

    Update tab icon css

--- components/TabBarIcon.tsx
+++ components/TabBarIcon.tsx
@@ -1,32 +1,60 @@
 import { Ionicons } from "@expo/vector-icons";
 import { Text, View } from "react-native";
 
-export const ActivePill = ({ iconName, label }: { iconName: string, label: string }) => (
-   
-    <View 
-        className="bg-[#10B981] rounded-full flex items-center justify-center p-6"
-        
-        style={{ paddingHorizontal: 16, height: 36, marginTop: -6 }} 
+export const ActivePill = ({
+  iconName,
+  label,
+}: {
+  iconName: string;
+  label: string;
+}) => (
+  <View className="items-center">
+    <View
+      className="bg-[#10B981] rounded-3xl items-center justify-center"
+      style={{ height: 60, width: 120, paddingHorizontal: 10 }}
     >
-        <Ionicons 
-            name={iconName as any} 
-            size={20} 
-            color="white" 
-            style={{ marginRight: 6 }} 
-        />
-        <Text className="text-white text-sm font-semibold">
-            {label}
-        </Text>
+      <Ionicons
+        name={iconName as any}
+        size={24}
+        color="white"
+        style={{ marginRight: 6 }}
+      />
+      <Text
+        className="text-white text-sm font-semibold"
+        style={{ marginTop: 8 }}
+      >
+        {label}
+      </Text>
     </View>
+  </View>
 );
 
-
-export const InactiveIcon = ({ name, color, size, label }: { name: string, color: string, size: number, label:string }) => (
-    <View className="items-center">
-       
-        <Ionicons name={`${name}-outline` as any} size={size} color={color} />
-        <Text style={{ fontSize: 12, fontWeight: '600', color: color, marginTop: 4 }}>
-            {label}
-        </Text>
-    </View>
+export const InactiveIcon = ({
+  name,
+  color,
+  size,
+  label,
+}: {
+  name: string;
+  color: string;
+  size: number;
+  label: string;
+}) => (
+  <View className="items-center">
+    <Ionicons name={`${name}-outline` as any} size={size} color={color} />
+    <Text
+      style={{
+        fontSize: 12,
+        fontWeight: "600",
+        color: color,
+        marginTop: 2,
+        textAlign: "center",
+        width: 150,
+        paddingHorizontal: 0,
+      }}
+      numberOfLines={1}
+    >
+      {label}
+    </Text>
+  </View>
 );
