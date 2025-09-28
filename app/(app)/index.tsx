e23311c4095ea01877e4a52eec6ceb2e70fca9a4
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-28 19:04:44 +0600

    Update main app chart css

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -7,7 +7,7 @@ import { BudgetDonutChart } from "@/components/BudgetDonutChart"; // Assuming yo
 export default function Index() {
   return (
     <SafeAreaView className="flex-1 bg-white">
-      <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
+      <View className="flex-1">
         <View className="flex-row justify-between items-center px-4 pt-4 pb-2">
           <View className="flex-row items-center">
             <Image source={require("../../assets/icons/logo-icon.png")} />
@@ -28,9 +28,10 @@ export default function Index() {
             </Text>
           </View>
         </View>
-        
-        <BudgetDonutChart />
-      </ScrollView>
+        <View className="flex-1 justify-center items-center px-4">
+          <BudgetDonutChart />
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
