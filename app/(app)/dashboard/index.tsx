49c57fbef0fd1a91bf6893d15c2afeea85f25c86
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-09-28 18:48:37 +0600

    Update dashboard screen

--- app/(app)/dashboard/index.tsx
+++ app/(app)/dashboard/index.tsx
@@ -1,70 +1,106 @@
-import { View, Text, ScrollView, Image, TouchableOpacity, FlatList, Dimensions } from 'react-native'
-import React, { useState } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
-import { EXPENSE_CATEGORY_ICONS, INCOME_CATEGORY_ICONS, MOCK_EXPENSES, MOCK_INCOME } from '@/Data'
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  TouchableOpacity,
+  FlatList,
+  Dimensions,
+} from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
+import {
+  EXPENSE_CATEGORY_ICONS,
+  INCOME_CATEGORY_ICONS,
+  MOCK_EXPENSES,
+  MOCK_INCOME,
+} from "@/Data";
+import { router } from "expo-router";
 
 type Category = {
+  id?: string;
   name: string;
-  icon:  string;
+  icon: string;
   amount: string;
 };
 type MOCK_DATA = {
+  id: string;
   name: string;
-  icon:  string;
+  icon: string;
   amount: number;
   date: string;
 };
 const screenWidth = Dimensions.get("window").width;
-const itemSpacing = 16; 
+const itemSpacing = 16;
 const numColumns = 4;
 const itemSize = (screenWidth - itemSpacing * (numColumns + 1)) / numColumns;
 
 const Index = () => {
-  const [isActive, setIsActive] = useState<'EXPENSES' | 'INCOME'>('EXPENSES'); 
+  const [isActive, setIsActive] = useState<"EXPENSES" | "INCOME">("EXPENSES");
 
-  const CATEGORY_ICONS = isActive === 'EXPENSES' 
-    ? EXPENSE_CATEGORY_ICONS 
-    : INCOME_CATEGORY_ICONS;
+  const CATEGORY_ICONS: Category[] =
+    isActive === "EXPENSES"
+      ? (EXPENSE_CATEGORY_ICONS as Category[])
+      : (INCOME_CATEGORY_ICONS as Category[]);
 
-    const transactionData = isActive === 'EXPENSES' ? MOCK_EXPENSES : MOCK_INCOME;
-  const listTitle = isActive === 'EXPENSES' ? 'Specific Cost' : 'Specific Earn';
-
+  const transactionData = isActive === "EXPENSES" ? MOCK_EXPENSES : MOCK_INCOME;
+  const listTitle = isActive === "EXPENSES" ? "Specific Cost" : "Specific Earn";
 
   const renderCategoryItem = ({ item }: { item: Category }) => (
-    <View style={{ width: itemSize, margin: itemSpacing / 2, alignItems: "center" }}>
+    <View
+      style={{ width: itemSize, margin: itemSpacing / 2, alignItems: "center" }}
+    >
       <View className="bg-gray-100 rounded-xl items-center justify-center p-3 w-full aspect-square shadow-sm">
-        <MaterialCommunityIcons name={item.icon as keyof typeof MaterialCommunityIcons.glyphMap} size={32} color="#49B58B" />
+        <MaterialCommunityIcons
+          name={item.icon as keyof typeof MaterialCommunityIcons.glyphMap}
+          size={32}
+          color="#49B58B"
+        />
       </View>
-        <Text className="text-[#49B58B] text-base font-semibold mt-1">{item.amount}</Text>
+      <Text className="text-[#49B58B] text-base font-semibold mt-1">
+        {item.amount}
+      </Text>
     </View>
   );
 
-  const renderTransactionItem = ({ item}:{item:MOCK_DATA}) => (
-    <TouchableOpacity 
-      className="flex-row justify-between items-center py-4 border-b border-gray-100" 
-      // onPress={handleItemPress}
+  const renderTransactionItem = ({ item }: { item: MOCK_DATA }) => (
+    <TouchableOpacity
+      className="flex-row justify-between items-center py-4 border-b border-gray-100"
+      onPress={() => handleItemPress(item)}
     >
       <View className="flex-row items-center">
         <View className="bg-gray-100 p-2 rounded-lg mr-3">
-            <MaterialCommunityIcons name={item.icon  as keyof typeof MaterialCommunityIcons.glyphMap} size={20} color="#3cb371" />
+          <MaterialCommunityIcons
+            name={item.icon as keyof typeof MaterialCommunityIcons.glyphMap}
+            size={20}
+            color="#3cb371"
+          />
         </View>
         <View>
-          <Text className="text-base font-medium text-gray-800">{item.name}</Text>
+          <Text className="text-base font-medium text-gray-800">
+            {item.name}
+          </Text>
           <Text className="text-xs text-gray-500">{item.date}</Text>
         </View>
       </View>
-      <Text 
-        className={`text-base font-semibold `}
-      >
-        ${item.amount}
-      </Text>
+      <Text className={`text-base font-semibold `}>${item.amount}</Text>
     </TouchableOpacity>
   );
+
+  const handleItemPress = (item: MOCK_DATA) => {
+    router.push({
+      pathname: "/(app)/dashboard/add-transaction",
+      params: {
+        transactionId: item.id,
+        transactionType: isActive,
+      },
+    });
+  };
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <ScrollView contentContainerStyle={{ paddingBottom: 20 }}>
-      
         <View className="flex-row justify-between items-center px-4 pt-4 pb-2">
           <View className="flex-row items-center">
             <Image source={require("../../../assets/icons/logo-icon.png")} />
@@ -79,15 +115,19 @@ const Index = () => {
         </View>
 
         <View className="flex-row justify-center py-4 px-12 gap-4">
-          <TouchableOpacity 
-            className={`w-1/2 py-3 rounded-lg ${isActive==='EXPENSES' ? "bg-[#49B58B]" : "bg-[#6C6B6E]"}`} 
-            onPress={()=>setIsActive('EXPENSES')}
+          <TouchableOpacity
+            className={`w-1/2 py-3 rounded-lg ${
+              isActive === "EXPENSES" ? "bg-[#49B58B]" : "bg-[#6C6B6E]"
+            }`}
+            onPress={() => setIsActive("EXPENSES")}
           >
             <Text className="text-white text-center font-bold">EXPENSES</Text>
           </TouchableOpacity>
-          <TouchableOpacity 
-            className={`w-1/2 py-3 rounded-lg ${isActive==='INCOME' ? "bg-[#49B58B]" : "bg-[#6C6B6E]"}`} 
-            onPress={()=>setIsActive('INCOME')}
+          <TouchableOpacity
+            className={`w-1/2 py-3 rounded-lg ${
+              isActive === "INCOME" ? "bg-[#49B58B]" : "bg-[#6C6B6E]"
+            }`}
+            onPress={() => setIsActive("INCOME")}
           >
             <Text className="text-white text-center font-bold">INCOME</Text>
           </TouchableOpacity>
@@ -99,24 +139,27 @@ const Index = () => {
             renderItem={renderCategoryItem}
             keyExtractor={(item) => item.name}
             numColumns={numColumns}
-            scrollEnabled={false} 
+            scrollEnabled={false}
             contentContainerStyle={{
               justifyContent: "center",
               padding: itemSpacing / 2,
             }}
           />
         </View>
-        <Text className="text-xl font-bold px-5 mt-4 mb-2 text-gray-800">{listTitle}</Text>
+        <Text className="text-xl font-bold px-5 mt-4 mb-2 text-gray-800">
+          {listTitle}
+        </Text>
         <FlatList
           data={transactionData}
           renderItem={renderTransactionItem}
-          keyExtractor={item => item.id}
+          keyExtractor={(item) => item.id}
           contentContainerStyle={{ paddingHorizontal: 20 }}
           ItemSeparatorComponent={() => <View className="h-px bg-gray-100" />}
+          scrollEnabled={false}
         />
       </ScrollView>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
